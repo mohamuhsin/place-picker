f21bc6d81c6a002ae4eafdbf7b67c209f6ebe3b5
mohamuhsin <muhsinohoho@outlook.com>
2024-07-25 12:19:52 +0300

    From our previous code we faced a problem of infite loop when we made a function a dependency, where a new function is always recreated, we used a new hook called useCallback which stores the innner function in memory and reuses it whenever the component function reexecutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useCallback, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -58,7 +58,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -69,7 +69,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
