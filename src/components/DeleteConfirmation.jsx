f6aeccb95a0ffbcb703ad65051e6b69b8b2d3203
mohamuhsin <muhsinohoho@outlook.com>
2024-07-26 20:43:26 +0300

    needed to show the user that there's a timer behind the scene, so we had to show a progress bar to show the user the progress of the timer, we used the progress element, initialized some state to manage the remainingTime and used setInterval() inside useEffect and also used a cleanup function to clear the interval when its complete

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("INTERVAL");
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     console.log("SET TIMER");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
-      console.log("Cleaning up timer");
       clearTimeout(timer);
     };
   }, [onConfirm]);
@@ -25,6 +39,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
